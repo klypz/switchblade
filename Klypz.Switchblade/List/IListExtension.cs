9a8d1c5dfe7526a2851d3f55caa1fbf97ecf71ee
Renato Moraes <klypz@klypz.com.br>
2018-04-15 12:07:46 -0300

    Pagination e ResultList

---
+++ Klypz.Switchblade/List/IListExtension.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Klypz.Switchblade.List
+{
+    public static class IListExtension
+    {
+        public static IQueryable<TSource> Pagination<TSource>(this IQueryable<TSource> self, int pageSize, int page)
+        {
+            int initial = page * pageSize;
+            int final = initial + pageSize;
+
+            if (final > self.Count())
+            {
+                final = self.Count();
+            }
+
+            return self.Skip(initial).Take(final);
+        }
+
+        public static IEnumerable<TSource> Pagination<TSource>(this IEnumerable<TSource> self, int pageSize, int page)
+        {
+            int initial = page * pageSize;
+            int final = initial + pageSize;
+
+            if (final > self.Count())
+            {
+                final = self.Count();
+            }
+
+            return self.Skip(initial).Take(final);
+        }
+    }
+}
---
+++ Klypz.Switchblade/List/ResultList.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Klypz.Switchblade.List
+{
+    public class ResultList<T>
+    {
+        public ResultList(int pageSize, int currentPage)
+        {
+            PageSize = pageSize;
+            CurrentPage = currentPage;
+        }
+
+        public int PageSize { get; private set; }
+        public int CurrentPage { get; private set; }
+        public int Pages { get; private set; }
+        public int Count { get; private set; }
+
+        public List<T> Result { get; private set; }
+
+        public void SetQuery(IQueryable<T> query)
+        {
+            int total = query.Count();
+            Pages = Math.Abs(total / PageSize) + (Math.Abs(total / PageSize) < total * 1.0 / PageSize * 1.0 ? 1 : 0);
+            Count = total;
+
+            Result = query.Pagination(PageSize, CurrentPage).ToList();
+        }
+
+        public void SetQuery(IEnumerable<T> query)
+        {
+            ProcesseInfoQuery(query.Count());
+            Result = query.Pagination(PageSize, CurrentPage).ToList();
+        }
+
+        private void ProcesseInfoQuery(int count)
+        {
+            int total = count;
+            Pages = Math.Abs(total / PageSize) + (Math.Abs(total / PageSize) < total * 1.0 / PageSize * 1.0 ? 1 : 0);
+            Count = total;
+        }
+    }
+}
